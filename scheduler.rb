f6df969d1238de0b1de9ca97bfd8555423d8c85a
Howard Hao <hhhao22@gmail.com>
2016-10-23 19:32:38 -0700

    Add test details

--- scheduler.rb
+++ scheduler.rb
@@ -68,7 +68,7 @@ class Schedule
   end
 
   def calcFitness(chromstr)
-    
+    return rand(0.0..100.0)
   end
 
   def decode(chrom)
@@ -172,14 +172,50 @@ class Schedule
       @population = nextgen
     end
     evalAllFitness
-    @schedule = decode(findFittestChrom)
+    decode(findFittestChrom)
   end
 end
 
 ##############################
-emp = [{name: 'Jack', role: :cook, avail: [[[6, 12], [15, 22]], [[5, 22]], [[3, 12], [14, 22]]]},
-       {name: 'Emily', role: :janitor, avail: [[[6, 12], [15, 22]], [[5, 22]], [[3, 12], [14, 22]]]}]
-shifts = [[{time: [9, 11], cook: 2}, {time: [13, 14], cook: 2}], [{time: [6, 10], janitor: 2}, {time: [10, 15], cook: 1}], [{time: [11, 15], cook: 1}], [], [], [], []]
+emp = [{name: 'Jack', role: :cook, avail: [[[6, 12], [15, 22]],
+                                           [[5, 22]],
+                                           [[3, 12], [14, 22]],
+                                           [[3, 22]],
+                                           [[3, 22]],
+                                           [[3, 22]],
+                                           [[3, 22]]]
+       },
+       {name: 'Emily', role: :janitor, avail: [[[6, 12], [15, 22]],
+                                               [[5, 22]],
+                                               [[3, 12], [14, 22]],
+                                               [[3, 22]],
+                                               [[3, 22]],
+                                               [[3, 22]],
+                                               [[3, 22]]]
+       },
+       {name: 'Francis', role: :server, avail: [[[6, 12]],
+                                                [[5, 22]],
+                                                [[3, 22]],
+                                                [[3, 22]],
+                                                [[3, 22]],
+                                                [[3, 22]],
+                                                [[3, 22]]]
+       }
+      ]
+shifts = [
+  [{time: [9, 11], cook: 2},
+   {time: [13, 14], cook: 2}],
+  [{time: [6, 10], janitor: 2, server: 3},
+   {time: [10, 15], cook: 1, server: 2}],
+  [{time: [11, 15], cook: 1},
+   {time: [10, 15], cook: 1, server: 2}],
+  [{time: [11, 15], cook: 1}],
+  [{time: [11, 15], cook: 1}],
+  [{time: [11, 15], cook: 1},
+   {time: [10, 15], cook: 1, server: 2}],
+  [{time: [11, 15], cook: 1}]
+]
+
 s = Schedule.new(emp, shifts)
 s.init
 p s.avail_table
